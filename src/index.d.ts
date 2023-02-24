2e88712e4072c3574458d72ba57048e96dd3d660
Pavle Vuković <vukovic.pavle@icloud.com>
2023-02-24 15:54:13 +0100

    validator middleware and type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,7 @@
-// Types
+import { NextFunction, Request, Response } from "express";
+import { Schema } from "joi";
+
+export function validatorMiddleware(
+  schema: Schema,
+  options?: { allowUnknown?: boolean }
+): (req: Request, res: Response, next: NextFunction) => void;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-// Entry file
+import { HttpError } from "@vukovicpavle/orange-http-error";
+import { NextFunction, Request, Response } from "express";
+import { Schema } from "joi";
+
+export function validatorMiddleware(
+  schema: Schema,
+  options?: { allowUnknown?: boolean }
+) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, options);
+    if (error) {
+      next(new HttpError(400, error.message));
+    } else {
+      next();
+    }
+  };
+}
